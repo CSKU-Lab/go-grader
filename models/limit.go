f20ee2098b32eb5f6b8e470a535228ef177dd8ae
SornchaiTheDev <sornchaithedev@gmail.com>
2025-02-22 17:35:16 +0700

    chore: update Limit field data type

--- models/limit.go
+++ models/limit.go
@@ -16,9 +16,9 @@ type Limit struct {
 	CPUTime      float32 `json:"cpuTime" arg:"--time"`
 	CPUExtraTime float32 `json:"cpuExtraTime" arg:"--extra-time"`
 	WallTime     float32 `json:"wallTime" arg:"--wall-time"`
-	Memory       float32 `json:"memory" arg:"--mem"`
-	Stack        float32 `json:"stack" arg:"--stack"`
-	MaxOpenFiles float32 `json:"maxOpenFiles" arg:"--open-files"`
+	Memory       int `json:"memory" arg:"--mem"`
+	Stack        int `json:"stack" arg:"--stack"`
+	MaxOpenFiles int `json:"maxOpenFiles" arg:"--open-files"`
 	MaxFileSize  float32 `json:"maxFileSize" arg:"--fsize"`
 	NetworkAllow bool    `json:"allowNetwork" arg:"--share-net"`
 }
