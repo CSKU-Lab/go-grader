6f119fae68924b605acd0e8f13abaf00e832bf13
SornchaiTheDev <sornchaithedev@gmail.com>
2025-04-08 01:12:55 +0700

    chore: implement simple grpc GetLanguages

--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -5,13 +5,30 @@ import (
 	"encoding/json"
 	"log"
 
+	pb "github.com/CSKU-Lab/go-grader/genproto/config/v1"
 	"github.com/CSKU-Lab/go-grader/infrastructure/queue"
 	"github.com/CSKU-Lab/go-grader/models"
 	"github.com/CSKU-Lab/go-grader/services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	ctx := context.Background()
+
+	conn, err := grpc.NewClient("host.docker.internal:8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to gRPC server: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewConfigServiceClient(conn)
+
+	languages, err := c.GetLanguages(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to get languages: %v", err)
+	}
+	log.Println("Languages: ", languages)
 
 	isolateService := services.NewIsolateService(ctx)
 	compileService := services.NewCompileService(ctx)
