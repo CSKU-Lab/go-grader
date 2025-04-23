2c1ac5321c65439651364b588c550e1f7f8f6c70
SornchaiTheDev <sornchaithedev@gmail.com>
2025-04-23 16:34:47 +0700

    chore: update isolate test script

--- scripts/isolate-test.sh
+++ scripts/isolate-test.sh
@@ -2,6 +2,10 @@
 
 echo "⌛ Starting isolate-test..."
 
+# cleanup
+docker stop isolate-test || true
+
 docker build -t isolate-test -f docker/isolate-test/Dockerfile .
 docker run --rm --privileged \
+    --name isolate-test \
     isolate-test
