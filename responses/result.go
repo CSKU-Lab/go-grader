a333a421f3b4e4895a18cbac88b8bc2f3def30a4
SornchaiTheDev <sornchaithedev@gmail.com>
2025-04-24 22:57:42 +0700

    chore: remove unused code

--- responses/result.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-import "github.com/CSKU-Lab/go-grader/domain/models"
-
-type Result struct {
-	SandboxMetadata *models.Metadata `json:"sandbox_metadata"`
-	Output          string           `json:"output"`
-	Error           string           `json:"error"`
-}
