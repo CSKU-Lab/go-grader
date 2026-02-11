df4931574d58fbf2936b5c32ddaf55249171f739
SornchaiTheDev <sornchaithedev@gmail.com>
2026-02-11 14:48:13 +0700

    feat: add json struct tag to grade_result.go

--- domain/models/grade_result.go
+++ domain/models/grade_result.go
@@ -3,24 +3,24 @@ package models
 import "github.com/CSKU-Lab/go-grader/domain/constants/execution"
 
 type TestCaseResult struct {
-	ID       string
-	Status   execution.Status
-	Input    string
-	Output   string
-	Message  string
-	WallTime float32
-	Memory   int32
+	ID       string           `json:"id"`
+	Status   execution.Status `json:"status"`
+	Input    string           `json:"input"`
+	Output   string           `json:"output"`
+	Message  string           `json:"message"`
+	WallTime float32          `json:"wall_time"`
+	Memory   int32            `json:"memory"`
 }
 
 type TestCaseGroupResult struct {
-	ID      string
-	Score   int32
-	Results []TestCaseResult
+	ID      string           `json:"id"`
+	Score   int32            `json:"score"`
+	Results []TestCaseResult `json:"results"`
 }
 
 type GradeResult struct {
-	Status               execution.Status
-	TestCaseGroupResults []TestCaseGroupResult
-	AvgWallTime          float32
-	AvgMemory            int32
+	Status               execution.Status      `json:"status"`
+	TestCaseGroupResults []TestCaseGroupResult `json:"test_case_group_results"`
+	AvgWallTime          float32               `json:"avg_wall_time"`
+	AvgMemory            int32                 `json:"avg_memory"`
 }
